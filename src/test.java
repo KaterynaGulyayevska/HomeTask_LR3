af0ac2a9aa8ec35d24c1bde8435c59aac703a869
Kateryna Gulyayevska <31535347+KaterynaGulyayevska@users.noreply.github.com>
2018-11-25 18:42:34 +0200

    Update test.java

--- src/test.java
+++ src/test.java
@@ -15,7 +15,7 @@ public class test {
                     System.out.println("You are wrong!");
                 } else {
 
-                System.out.println("Game over");
+                System.out.println("Game over!try again!");
                 }
             }
         }

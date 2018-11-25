52f04466f6f8be62d515bdac28dffb0cba04fc11
kateg <k.gulyayevska@gmail.com>
2018-11-25 18:40:05 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/test.java

--- src/test.java
+++ src/test.java
@@ -15,7 +15,7 @@ public class test {
                     System.out.println("You are wrong!");
                 } else {
 
-                System.out.println("Do you want to play again?");
+                System.out.println("Do you want to play?");
                 }
             }
         }
